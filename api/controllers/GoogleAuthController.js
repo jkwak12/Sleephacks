8aa417d20c56d274a580a7471ffde365f97d0319
Annelies <annelies.odermann@gmail.com>
2014-09-06 18:23:19 -0500

    The google calendar api is a clusterfsk that should be put out of its misery

--- api/controllers/GoogleAuthController.js
+++ api/controllers/GoogleAuthController.js
@@ -1,4 +1,5 @@
 var keys = require('../../config/keys.js')
+var request = require('request')
 var google = require('googleapis')
 var OAuth2 = google.auth.OAuth2
 
@@ -32,12 +33,61 @@ module.exports = {
   },
 
   getBusyTimes: function(req, res) {
-    calendar.freebusy.query({'timeMin': '2014-08-06T10:00:00-07:00', 'timeMax': '2014-08-12T10:00:00-07:00'}, function(err, list) {
-      if (err) sails.log.error(err)
-      console.log(list)
-      //res.send(list.items)
-      res.send(list)
+
+    var otherAuth = {
+      token: oauth2Client.credentials.access_token,
+      consumer_key: keys.config.google.key,
+      consumer_secret: keys.config.google.secret
+    }
+
+    
+    
+    var options = {
+      url: 'https://www.googleapis.com/calendar/v3/freeBusy',
+      method: "POST",
+      json: '{timeMin: \"2014-08-06T04:47:09Z\", timeMax: \"2014-08-12T04:47:09Z\", items: ['+ req.query.calendars + ']}',
+      qs: {key: keys.config.google.key},
+      oauth: oauth2Client,
+      'X-JavaScript-User-Agent': 'google-api-javascript-client/1.0.0-alpha'
+    }
+
+    console.log('{timeMin: \"2014-08-06T04:47:09Z\", timeMax: \"2014-08-12T04:47:09Z\", items: ['+ req.query.calendars + ']}')
+
+    request(options, function(err, response, body) {
+      res.send(body)
     })
+
+    
+
+    /*
+    
+    var options = {
+      url: 'https://www.googleapis.com/calendar/v3/calendars/' + req.query.calendarId + '/events',
+      qs: {key: keys.config.google.key},
+      method: 'GET',
+      oauth: oauth2Client,
+      'X-JavaScript-User-Agent': 'google-api-javascript-client/1.0.0-alpha'
+    }
+
+    request(options, function(err, response, body) {
+      res.send(body)
+    })
+
+*/
+
+
+
+    
+
+    // req.query = {timeMin: '2014-08-06T04:47:09Z', timeMax: '2014-08-12T04:47:09Z', index: req.query.calendars}
+    // console.log(req.query)
+    // var specialCal = google.calendar({ version: 'v3', auth: oauth2Client, body: '{timeMin: 2014-08-06T04:47:09Z, timeMax: 2014-08-12T04:47:09Z}' })
+    // specialCal.freebusy.query({timeMin: '2014-08-06T04:47:09Z', timeMax: '2014-08-12T04:47:09Z', items: req.query.calendars}, function(err, list) {
+    //   if (err) sails.log.error(err)
+    //   console.log(list)
+    //   //res.send(list.items)
+    //   res.send(list)
+    // })
   }
 
 
