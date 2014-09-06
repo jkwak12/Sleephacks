ce0e6cd8312fba9a94111a9f2bb26c4655e14ecf
Jiwon Kwak <jiwonkwak@0587360113.wireless.umich.net>
2014-09-06 14:19:39 -0400

    fixed merge conflicts

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports.config = {
 
   google: {
@@ -7,6 +6,3 @@ module.exports.config = {
   }
 
 }
-=======
-module.exports.config = { google: { key: '1099490248787-28ibl9ugg3qlceuscme4sf2r0uhlhr3o.apps.googleusercontent.com', secret: 'XT78oJyJaDHJx18rkKfuPGL7' } }
->>>>>>> 9636cc195f4111de59c8112004dedc43980ff0c7
