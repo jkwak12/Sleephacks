407c4250e4f56609251065d1b1b1f2e62ed3307a
Annelies <annelies.odermann@gmail.com>
2014-09-06 19:41:58 -0500

    THINGS AND

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,8 @@ module.exports.config = {
   google: {
     key: '1099490248787-28ibl9ugg3qlceuscme4sf2r0uhlhr3o.apps.googleusercontent.com',
     secret: 'XT78oJyJaDHJx18rkKfuPGL7',
-    serverKey: 'AIzaSyCqEcTdxUEy6j0N7HKi7KCMgYdrQjN9fWk'
+    serverKey: 'AIzaSyCqEcTdxUEy6j0N7HKi7KCMgYdrQjN9fWk',
+    browserKey: 'AIzaSyAkyVbxaHStT6PyuelPkWquJNWV50Hk4Ys'
   }
 
 }
